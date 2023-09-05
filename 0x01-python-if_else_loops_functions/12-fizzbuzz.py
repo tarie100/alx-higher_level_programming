aa9b7772d2d7a51741b3cb0735c95778b725b513
tarie100 <tarie.school@gmail.com>
2023-09-05 12:46:45 -0700

    ifs_loops_c

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for num in range(1, 101):
+        if (num % 3 == 0):
+            print("Fizz")
+        if (num % 5 == 0):
+            print("Buzz")
+        elif ((num % 3 == 0) and (num % 5 == 0)):
+            print("FizzBuzz")
