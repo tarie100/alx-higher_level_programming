58554b09174598cc83f5acd4ebcf862107467cb3
tarie100 <tarie.school@gmail.com>
2023-09-07 12:48:33 -0700

    2-argd

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 if __name == "__main__":
     import sys
-for args in range(len(sys.argv)):
-    if args == 0:
-        print("0 arguments.")
-    elif args == 1:
-        print("1 argument.")
-    else:
-        print("{} arguments:".format(args))
-        for i in range(args):
-            print("{[}: {}".format(i + 1, sys.argv[i + 1]))
+args = len(sys.argv) - 1
+if args == 0:
+    print("0 arguments.")
+elif args == 1:
+    print("1 argument.")
+else:
+    print("{} arguments:".format(args))
+    for i in range(args):
+        print("{[}: {}".format(i + 1, sys.argv[i + 1]))
