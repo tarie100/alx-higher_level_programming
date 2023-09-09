96ca06faff1f7f8344c084c9128a51a62aa220e2
tarie100 <tarie.school@gmail.com>
2023-09-09 02:28:09 -0700

    element

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for numbers in len(my_list):
+    for numbers in range(len(my_list)):
         print("{:d}".format(num))
--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx in my_list < 0:
-        return None
-    elif idx in my_list >= len((my_list)- 1):
+    if idx in my_list < 0 or idx >(len(my_list) - 1):
         return None
     return (my_list[idx])
