16f7e82d68034e1d07c3cd8840d93c8699e47338
tarie100 <tarie.school@gmail.com>
2023-09-10 10:09:22 -0700

    8-mut_returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        return "Length: 0 - First character: None"
-    else:
-        return "Length: {:d} - First character: {:s}".format(len(sentence), sentence[0])
+        return (0, None)
+    return (len(sentence), sentence[0])
