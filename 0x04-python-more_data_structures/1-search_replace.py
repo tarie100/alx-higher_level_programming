fb694719a4bbbb8e1dbdd4bfa03ddf0665155892
tarie100 <tarie.school@gmail.com>
2023-09-13 07:54:59 -0700

    1-search_replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,11 +2,11 @@
 def search_replace(my_list, search, replace):
     count_dict = {}
     new_list = []
-    for search in my_list:
-        if num in count_dict:
+    for item in my_list:
+        if item in count_dict:
             new_list.append('rep')
         else:
-            count_dict[search] = 1
+            count_dict[item] = 1
             new_list.append(replace)
     print(my_list)
     print(new_list)
