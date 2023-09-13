6d06bf410330e20d7600751bbdedd99f80cff4f7
tarie100 <tarie.school@gmail.com>
2023-09-13 12:43:10 -0700

    6-sorted_dict

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    uniq_nums = set()
+    uniq_nums = set(my_list)
     total = 0
     for num in my_list:
-        if num not in uniq_nums:
-            total += uniq_nums.add(num)
+        total += i
     return total
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,5 +2,5 @@
 def print_sorted_dictionary(a_dictionary):
     my_keys = list(a_dictionary.keys())
     my_keys.sort()
-    sorted_dict = {i: a_dictionary[i] for i in my_keys}
-    print(sorted_dict)
+    for i in my_keys:
+        print("{}: {}".format(i, a_dictionary.get(i)))
