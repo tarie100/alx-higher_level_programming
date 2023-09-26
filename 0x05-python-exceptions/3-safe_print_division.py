da819057a7eea33b4d50e35566e25f5f357ff852
tarie100 <tarie.school@gmail.com>
2023-09-26 02:51:21 -0700

    all

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    quotient = (a / b)
     try:
-        print("{}".format(quotient, end=""))
-    except ArithmeticError:
-        raise
-    else:
-        return None
+        quotient = a / b
+    except (TypeError, ZeroDivisionError):
+        quotient = None
     finally:
-        print()
-        return quotient
+        print("Inside result: {}".format(quotient))
+    return (quotient)
--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def raise_exception():
-    num1 = 0
-    mum2 = 5
-    ans = 0 / 5
-    try:
-        print(ans)
-    except ZeroDivisionError:
-        raise
+    raise TypeError
