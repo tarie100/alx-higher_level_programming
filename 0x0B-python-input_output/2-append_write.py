4a0813ab126c2129882080ee923933b1b8b77eef
tarie100 <tarie.school@gmail.com>
2023-10-11 05:51:02 -0700

    2-append

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,4 +3,4 @@ def append_write(filename="", text=""):
     """define func"""
     with open(filename, "a", encoding="utf8") as a_file:
         """open file and append"""
-        return a_file.a(text)
+        return a_file.write(text)
