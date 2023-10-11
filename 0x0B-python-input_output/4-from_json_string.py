3befacc3ead08bd8ad275a7407e3c0949e337935
tarie100 <tarie.school@gmail.com>
2023-10-11 12:31:04 -0700

    4-from_json

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""define func"""
+def from_json_string(my_str):
+    """iport json"""
+    import json
+    """returns py data struct. rep by json"""
+    return json.loads(my_str)
