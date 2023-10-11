01519952f5978c0076a62421fea2fb2685b0c532
tarie100 <tarie.school@gmail.com>
2023-10-11 05:31:09 -0700

    0-read_file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""defining file reader"""
+def read_file(filename=""):
+    """rep function"""
+    with open(filename, encoding="utf-8") as my_file:
+        """open file with utf8 encoder"""
+        print(my_file.read, end="")
