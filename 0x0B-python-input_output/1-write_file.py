0d41247cf2b5205f244564a85c28b1bc935d1ee6
tarie100 <tarie.school@gmail.com>
2023-10-11 05:43:53 -0700

    1-write_file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,8 +2,6 @@
 """define func"""
 def write_file(filename="", text=""):
     """open file with utf8"""
-    with open(filename, encoding="utf-8") as w_file:
+    with open(filename, "w", encoding="utf-8") as w_file:
         """write to file"""
-        w_file.write(text)
-        """return written chars"""
-        return w_file.tell()
+        return w_file.write(text)
