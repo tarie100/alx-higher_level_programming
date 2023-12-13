6049a6d4daf49b22e4e57732d5862feaeea09c60
tarie100 <tarie.school@gmail.com>
2023-12-13 06:54:22 -0800

    Rectangle #3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor(w, h) {
+    if (w > 0 && Number.isInteger(w) && h > 0 && Number.isInteger(h)) {
+      this.width = w;
+      this.height = h;
+    } else {
+      Object.assign(this, {});
+    }
+  }
+
+  print() {
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    } else {
+      console.log('Empty object');
+    }
+  }
+}
