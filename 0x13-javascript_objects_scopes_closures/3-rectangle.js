c194a26fee0668e994e1fca99ea70677371a738b
tarie100 <tarie.school@gmail.com>
2023-12-13 11:43:56 -0800

    fix semistandard errors

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && Number.isInteger(w) && h > 0 && Number.isInteger(h)) {
       this.width = w;
       this.height = h;
@@ -9,7 +9,7 @@ class Rectangle {
     }
   }
 
-  print() {
+  print () {
     if (this.width && this.height) {
       for (let i = 0; i < this.height; i++) {
         console.log('X'.repeat(this.width));
