e4fd3cae8e8dd93b291ee4e28eabbbcbf4c93873
tarie100 <tarie.school@gmail.com>
2024-01-14 13:06:28 -0800

    2. cURL Method

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a request to that URL, and displays the size of the body of the response
-read -p
-response = $(curl -s -o /dev/null -w "%{size_download}" "$url")
-echo "Size of the response body: $response bytes"
+curl -sI "$1" | awk '/Content-Length/{print $2}'
